48155c42b811487bda5e5b9794495b94bbfd803b
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-04-22 05:48:29 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ games { 'gta5' }
 
 author 'HenkW'
 description 'Simple announcement script with okok, mythic, chat and esx notify support'
-version '1.0.3'
+version '1.0.4'
 
 server_scripts {
     'server/main.lua',
